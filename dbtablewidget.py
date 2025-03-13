e93fd6a7408cb03666327888c39636a350fcaeed
alunoUFU <alunoufu@ufu.br>
2025-03-13 14:41:28 -0300

    refactor ui_main.py

--- dbtablewidget.py
+++ dbtablewidget.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QTableWidget, QLineEdit, QComboBox, QTableWidgetItem
-from PySide6.QtGui import QColor
+from PySide6.QtGui import QColor, QFont
 from PySide6.QtCore import Qt
 from functools import partial
 from hrt.hrt_data import HrtData
@@ -7,13 +7,25 @@ from hrt.hrt_enum import hrt_enum
 from hrt.hrt_bitenum import hrt_bitEnum
 
 class DBTableWidget(QTableWidget):
-    def __init__(self):
-        super().__init__()    
+    def __init__(self, parent):
+        super().__init__(parent=parent)    
         
     def setBaseData(self, hrt_data: HrtData):
         self.hrt_data = hrt_data
         self.hrt_data.data_updated.connect(self.redraw)  # Conecta sinal de atualização
         self.cellChanged.connect(self.on_cell_changed)  # Detecta edições na tabela  
+        
+        header_font = QFont("Arial", 12, QFont.Bold)  # Fonte maior e em negrito
+        self.horizontalHeader().setFont(header_font)
+        self.verticalHeader().setFont(header_font)        
+        # Criar uma fonte com tamanho 14
+        # fonte = QFont("Arial", 14)
+        # # Aplicar a fonte na tabela inteira
+        # self.setFont(fonte)        
+        self.redraw()
+        
+    def changeType(self, state:bool):
+        self.state = not state
         self.redraw()
         
     def redraw(self):   
@@ -21,8 +33,11 @@ class DBTableWidget(QTableWidget):
         rows, cols = self.df.shape
         self.blockSignals(True)  # Bloqueia sinais para evitar loops infinitos
         self.setRowCount(rows)
-        self.setColumnCount(cols)
-        self.setHorizontalHeaderLabels(self.df.columns)
+        self.setColumnCount(cols-1)
+        self.setHorizontalHeaderLabels([item for item in self.df.columns if item != "NAME"])
+        self.setVerticalHeaderLabels(self.df.iloc[:,0])        
+        # Ajuste automático das colunas ao conteúdo
+                
         widget_row_types = self.df['TYPE']    
         for row in range(rows): 
             if any(x in widget_row_types[row][:4] for x in ["PACK", "ASCI", "UNSI", "FLOA", "INTE", "INDE"]):  # "QLineEdit"
@@ -32,16 +47,16 @@ class DBTableWidget(QTableWidget):
             else:
                 row_type = 0
               
-            for col in range(cols): 
-                data = self.hrt_data.get_variable_with_pos(row, col, machineValue=(col <= 2)) 
+            for col in range(1, cols): 
+                data = self.hrt_data.get_variable_with_pos(row, col, machineValue=(col <= 2) or self.state) 
                 cell_value = f"{data:.2f}" if isinstance(data, float) else str(data)
 
-                if col > 2 and row_type == 2:
+                if col == 2 or ((col >= 2 or self.state) and row_type == 2):
                     widget = QLineEdit()
                     widget.setStyleSheet("QLineEdit { background-color: white; }")
                     widget.setText(cell_value)
                     widget.editingFinished.connect(partial(self.on_edit_finished, widget, row, col))
-                    self.setCellWidget(row, col, widget)
+                    self.setCellWidget(row, col-1, widget)
 
                 elif col > 2 and row_type == 1:
                     widget = QComboBox()
@@ -52,13 +67,14 @@ class DBTableWidget(QTableWidget):
                     widget.addItems(dados)
                     widget.setCurrentText(cell_value)
                     widget.currentIndexChanged.connect(partial(self.on_combo_changed, widget, row, col))
-                    self.setCellWidget(row, col, widget)
+                    self.setCellWidget(row, col-1, widget)
                 
                 else:
                     item = QTableWidgetItem(cell_value)
                     item.setFlags(item.flags() & ~Qt.ItemFlag.ItemIsEditable)  # Não editável
                     item.setBackground(QColor("#D3D3D3"))  # Define fundo cinza claro
-                    self.setItem(row, col, item)
+                    self.setItem(row, col-1, item)
+                self.setColumnWidth(col-1, 100)
 
         self.blockSignals(False)  # Libera sinais após a configuração da tabela
         self.viewport().update()  # Atualiza a interface
@@ -66,14 +82,14 @@ class DBTableWidget(QTableWidget):
     def on_edit_finished(self, widget, row, col):
         """Atualiza os dados ao editar um QLineEdit."""
         value = widget.text()
-        self.hrt_data.set_variable_with_pos(value, row, col, machineValue=(col <= 2))
+        self.hrt_data.set_variable_with_pos(value, row, col, machineValue=(col <= 2) or self.state)
 
     def on_combo_changed(self, widget, row, col, _):
         """Atualiza os dados ao mudar um QComboBox."""
         value = widget.currentText()
-        self.hrt_data.set_variable_with_pos(value, row, col, machineValue=(col <= 2))
+        self.hrt_data.set_variable_with_pos(value, row, col, machineValue=(col <= 2) or self.state)
 
     def on_cell_changed(self, row, column):
         """Atualiza o Excel sempre que uma célula for editada na interface."""
         value = self.tableWidget.item(row, column).text()
-        self.hrt_data.set_variable_with_pos(value, row, column, machineValue=(column <= 2))
+        self.hrt_data.set_variable_with_pos(value, row, column, machineValue=(column <= 2) or self.state)
--- uis/ui_main.py
+++ uis/ui_main.py
@@ -15,8 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QMainWindow, QSizePolicy,
-    QStatusBar, QTabWidget, QWidget)
+from PySide6.QtWidgets import (QApplication, QButtonGroup, QHBoxLayout, QLCDNumber,
+    QLabel, QMainWindow, QRadioButton, QSizePolicy,
+    QSpacerItem, QStatusBar, QTabWidget, QVBoxLayout,
+    QWidget)
 
 from ctrlglwidget import CtrlGLWidget
 from dbtablewidget import DBTableWidget
@@ -32,11 +34,122 @@ class Ui_MainWindow(object):
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.tabWidget = QTabWidget(self.centralwidget)
         self.tabWidget.setObjectName(u"tabWidget")
-        self.oldDBTableWidget = DBTableWidget()
+        self.tabdbase = QWidget()
+        self.tabdbase.setObjectName(u"tabdbase")
+        self.verticalLayout_3 = QVBoxLayout(self.tabdbase)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.radioButtonHex = QRadioButton(self.tabdbase)
+        self.buttonGroup = QButtonGroup(MainWindow)
+        self.buttonGroup.setObjectName(u"buttonGroup")
+        self.buttonGroup.addButton(self.radioButtonHex)
+        self.radioButtonHex.setObjectName(u"radioButtonHex")
+        self.radioButtonHex.setChecked(True)
+
+        self.horizontalLayout_3.addWidget(self.radioButtonHex)
+
+        self.radioButtonHrt = QRadioButton(self.tabdbase)
+        self.buttonGroup.addButton(self.radioButtonHrt)
+        self.radioButtonHrt.setObjectName(u"radioButtonHrt")
+
+        self.horizontalLayout_3.addWidget(self.radioButtonHrt)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer_2)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_3)
+
+        self.oldDBTableWidget = DBTableWidget(self.tabdbase)
         self.oldDBTableWidget.setObjectName(u"oldDBTableWidget")
-        self.tabWidget.addTab(self.oldDBTableWidget, "")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.oldDBTableWidget.sizePolicy().hasHeightForWidth())
+        self.oldDBTableWidget.setSizePolicy(sizePolicy)
+
+        self.verticalLayout_3.addWidget(self.oldDBTableWidget)
+
+        self.tabWidget.addTab(self.tabdbase, "")
         self.oldCtrlGLWidget = CtrlGLWidget()
         self.oldCtrlGLWidget.setObjectName(u"oldCtrlGLWidget")
+        self.horizontalLayout_7 = QHBoxLayout(self.oldCtrlGLWidget)
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_6.addItem(self.horizontalSpacer)
+
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label = QLabel(self.oldCtrlGLWidget)
+        self.label.setObjectName(u"label")
+        font = QFont()
+        font.setPointSize(20)
+        self.label.setFont(font)
+
+        self.horizontalLayout_2.addWidget(self.label)
+
+        self.lcdNumber_2 = QLCDNumber(self.oldCtrlGLWidget)
+        self.lcdNumber_2.setObjectName(u"lcdNumber_2")
+
+        self.horizontalLayout_2.addWidget(self.lcdNumber_2)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.label_2 = QLabel(self.oldCtrlGLWidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setFont(font)
+
+        self.horizontalLayout_4.addWidget(self.label_2)
+
+        self.lcdNumber_3 = QLCDNumber(self.oldCtrlGLWidget)
+        self.lcdNumber_3.setObjectName(u"lcdNumber_3")
+
+        self.horizontalLayout_4.addWidget(self.lcdNumber_3)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_4)
+
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.label_3 = QLabel(self.oldCtrlGLWidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setFont(font)
+
+        self.horizontalLayout_5.addWidget(self.label_3)
+
+        self.lcdNumber_4 = QLCDNumber(self.oldCtrlGLWidget)
+        self.lcdNumber_4.setObjectName(u"lcdNumber_4")
+
+        self.horizontalLayout_5.addWidget(self.lcdNumber_4)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_5)
+
+
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer)
+
+
+        self.horizontalLayout_6.addLayout(self.verticalLayout_2)
+
+
+        self.horizontalLayout_7.addLayout(self.horizontalLayout_6)
+
         self.tabWidget.addTab(self.oldCtrlGLWidget, "")
 
         self.horizontalLayout.addWidget(self.tabWidget)
@@ -47,6 +160,7 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
+        self.radioButtonHrt.toggled.connect(self.oldDBTableWidget.changeType)
 
         self.tabWidget.setCurrentIndex(0)
 
@@ -56,7 +170,12 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.oldDBTableWidget), QCoreApplication.translate("MainWindow", u"DBase", None))
+        self.radioButtonHex.setText(QCoreApplication.translate("MainWindow", u"Hex", None))
+        self.radioButtonHrt.setText(QCoreApplication.translate("MainWindow", u"Hart", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabdbase), QCoreApplication.translate("MainWindow", u"DBase", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"PV", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"SP", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"MV", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.oldCtrlGLWidget), QCoreApplication.translate("MainWindow", u"CtrlN\u00edvel", None))
     # retranslateUi
 
