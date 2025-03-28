02827f8902abbaf5cc9ee5520a377ed90b4d6488
alunoUFU <alunoufu@ufu.br>
2025-03-28 15:40:18 -0300

    refactor template_db.py

--- db/old/creatDB.py
+++ db/old/creatDB.py
@@ -1,10 +1,10 @@
-from hrt_banco import hrt_banco
+from template_db import hrt_banco, mb_banco
 import pandas as pd
 import sqlite3
 
 def hrt_crateDB(db_name: str,table_name: str):
     # Colunas adicionais
-    extra_columns = ['FI100CA', 'FV100CA', 'FV100AR', 'FV100AR', 'FI100V', 'PI100V', 'FI100A', 'FV100A', 'PI100A', 'LI100', 'TI100']
+    extra_columns = ['FV100CA', 'FI100CA', 'FV100AR', 'FI100AR', 'TI100', 'FI100V', 'PI100V', 'LI100', 'PI100A', 'FV100A', 'FI100A']
     # Converter o dicionário em DataFrame com colunas repetidas
     rows = []
     for key, val in hrt_banco.items():
@@ -18,10 +18,10 @@ def hrt_crateDB(db_name: str,table_name: str):
 
 def mb_crateDB(db_name: str,table_name: str):
     rows = []    
-    for key, val in hrt_banco.items():
-        row = [key, val[0], val[1], val[2] + val[3]]
+    for key, val in mb_banco.items():
+        row = [key, val[0], val[1], val[2], val[3]]
         rows.append(row)        
-    columns = ['NAME', 'BYTE_SIZE', 'TYPE', 'ADDRESS', 'CLP100']
+    columns = ['NAME', 'TYPE', 'MB_POINT', 'ADDRESS', 'CLP100']
     df = pd.DataFrame(rows, columns=columns)
     with sqlite3.connect(db_name) as conn:
         df.to_sql(table_name, conn, if_exists='replace', index=False)
@@ -29,5 +29,5 @@ def mb_crateDB(db_name: str,table_name: str):
 
 # Exemplo de uso
 if __name__ == '__main__':
-    # hrt_crateDB('db/banco.db','HART_tabela')
+    hrt_crateDB('db/banco.db','HART_tabela')
     mb_crateDB('db/banco.db','MODBUS_tabela')    
